73f44a4f898db2e3f4dc936db9b4537f74964fc9
Yundi Shi <yundi@hebb.ia.unc.edu>
2010-07-08 14:11:35 -0400

    deleted some unused EMS seg python scripts

--- DoRegEMS.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-
-# Script to run after skull stripping, atlas building and parcellation propogation
-# 1) tissue segmentation using the newly build atlas
-# 2) Apply affine registration (Nreg / RegisterImages) & Warp field to Parcellation
-# 3) Apply affine registration (Nreg / RegisterImages) & Warp field to SubCortical Segmentation  
-#
-# Yundi Shi  
